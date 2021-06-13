e0b99dbd4a7c83064391ce0097a3c192eb68d30a
Silvana K <84741857+sikoh@users.noreply.github.com>
2021-06-12 23:03:50 -0400

    stretch 2

--- index.js
+++ index.js
@@ -341,16 +341,19 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-function getHTML(/*Code Here*/) {
+function getHTML(/* Code here */) {
   /* Code here */
 }
 
 /* 💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪
 Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */) {
-  /* Code here */
+function randomize(array) {
+  array.sort(() => Math.random() - 0.5);
 }
+let array = [1, 2, 3, 4, 5];
+randomize(array);
+console.log(array);
 
 /* 💪💪💪💪💪💪 STRETCH 3: 💪💪💪💪💪💪
  Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
